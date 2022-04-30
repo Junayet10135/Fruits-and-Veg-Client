2635e45d739cdf00015698f3599fb6180d42363e
JunayetTamim <junayettamim186@gmail.com>
2022-04-30 23:51:00 +0600

    fake data load

--- src/Pages/Home/Product/Product.js
+++ src/Pages/Home/Product/Product.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Product.css'
 
-const Product = () => {
+const Product = ({ inventory }) => {
+    const { name, img, price, quantity, Supplier, description } = inventory;
     return (
-        <div>
-            
+        <div className='inventory-container mb-5 g-5 col-sm-12 col-md-6 col-lg-4 rounded'>
+            <img src={img} className="card-img-top" alt="..." />
+            <div className="card-body text-start">
+                <h5 className="card-title">{name}</h5>
+                <p className="card-text"><strong>Price: $</strong> {price}</p>
+                <p className="card-text"><strong>Quantity:</strong> {quantity}</p>
+                <p className="card-text"><strong>Supplier Name:</strong> {Supplier}</p>
+                <p><small>{description}</small></p>
+                <button className='button d-block mx-auto'> Update</button>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Products.css'
 
 const Products = () => {
+    const [inventorys, setInventorys] = useState([]);
+
+    useEffect(()=>{
+        fetch('inventory.json')
+        .then(res=>res.json())
+        .then(data => setInventorys(data))
+    },[]);
     return (
         <div className='container'>
-            <h2>Here products section</h2>
+            <h2>Here products section :{inventorys.length}</h2>
+            <div className='row'>
+                {
+                    inventorys.slice(0,6).map(inventory => <Product
+                    key={inventory.id}
+                    inventory = {inventory}
+                    ></Product>)
+                }
+            </div>
         </div>
     );
 };
